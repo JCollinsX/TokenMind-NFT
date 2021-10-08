8a77ff58c8b922b15dc59c769082225410346479
Jenson <jenson.collins@hotmail.com>
2021-10-08 16:03:08 +0900

    NFT token

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,9 +8,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
     address contractAddress;
 
     constructor(address marketplaceAddress) ERC721("TokenMind", "TMFT") {
         contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,7 +9,10 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    },
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
@@ -22,7 +25,6 @@ module.exports = {
       url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
-    */
   },
   solidity: {
     version: "0.8.4",
